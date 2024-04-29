e9fc742829d83ce1aa1c13748d48f07aa5a7e650
Jhon C. Valencia <carlosmcms725@hotmail.com>
2024-04-28 22:11:50 -0500

    Responsive css added

---
+++ js/commentScript.js
@@ -0,0 +1,12 @@
+const fetchComments = () => {
+  fetch('https://jsonplaceholder.typicode.com/comments')
+  .then((response) => response.json())
+  .then(data => {
+    for (let i = 0; i < 3; i++) {
+      let comment = data[i]
+      document.querySelector(`#testimonial-name-${i}`).textContent = comment.name
+      document.querySelector(`#testimonial-mail-${i}`).textContent = comment.email
+      document.querySelector(`#testimonial-comment-${i}`).textContent = comment.body
+    }
+  })
+}
